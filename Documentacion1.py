5f7be2a24a44658d0b936096f1dc648428b70135
LMMR96 <launchx07712@innovaccion.mx>
2022-02-20 22:18:45 -0600

    Se agrega el proyecto

---
+++ Documentacion1.py
@@ -0,0 +1,19 @@
+# program.py
+sum = 1 + 2
+print(sum)
+print('Hola desde la consola')
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
+
